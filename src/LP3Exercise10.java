abef58b7c8ed86a5720aec64d54ada970f9503bd
Alex <alexnouansacksy@gmail.com>
2023-09-05 23:00:27 -0500

    Update (Desktop)

--- src/LP3Exercise10.java
+++ src/LP3Exercise10.java
@@ -1,2 +1,23 @@
+// Alex Nouansacksy
+// LP3-10 w/classes
+// 09/05/2023
+import java.util.*;
+import static java.lang.System.out;
 public class LP3Exercise10 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        out.print("Enter the number of burgers: ");
+        int burgers = keyboard.nextInt();
+        out.print("Enter the number of fries: ");
+        int fries = keyboard.nextInt();
+        out.print("Enter the number of sodas: ");
+        int sodas = keyboard.nextInt();
+        out.print("Enter amount tendered: ");
+        double cash = keyboard.nextDouble();
+
+
+        ClLP3Exercise10 wow = new ClLP3Exercise10(burgers, fries, sodas, cash);
+        wow.calculate();
+        wow.print();
+    }
 }
--- src/LP3Exercise6.java
+++ src/LP3Exercise6.java
@@ -1,5 +1,29 @@
+// Alex Nouansacksy
+// LP 3-6
+// 09/05/2023
+import java.util.*;
+import static java.lang.System.out;
 public class LP3Exercise6 {
     public static void main(String[] args) {
-
+        Scanner keyboard = new Scanner(System.in);
+        out.print("Enter a three-digit number: ");
+        int num = keyboard.nextInt();
+        int hundreds = num / 100;
+        num %= 100;
+        int tens = num / 10;
+        num %= 10;
+        int ones = num;
+        out.println("The number in the hundreds place is: " + hundreds);
+        out.println("The number in the tens place is: " + tens);
+        out.println("The number in the ones place is: " + ones);
     }
 }
+/*output
+
+Enter a three-digit number: 256
+The number in the hundreds place is: 2
+The number in the tens place is: 5
+The number in the ones place is: 6
+
+Process finished with exit code 0
+ */
