e6196ea4f04290b9316b5821939e8c16f9eee08d
Alex <alexnouansacksy@gmail.com>
2023-10-05 21:34:44 -0500

    Update

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,5 +1,9 @@
+// Alex Nouansacksy
+// Hello World
+// 09/01/2023
+import static java.lang.System.out;
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello World!!");
+        out.print("Hello World!!");
     }
 }
