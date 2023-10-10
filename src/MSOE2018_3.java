a42e1934f53530bf09fad0d7043065e5f301b879
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2023-10-10 09:02:11 -0500

    Update

--- src/MSOE2018_3.java
+++ src/MSOE2018_3.java
@@ -27,40 +27,18 @@ public class MSOE2018_3 {
         } else if (degrees == 90) {
             x = 1;
             y = 0;
-        }
-        else if (degrees > 270){
-            degrees -= 270;
-            x = Math.cos(test);
-            y = Math.sin(test);
-
-            double temp =  x;
-            x = y;
-            y = temp;
-
-        }
-        else if (degrees > 180) {
-            degrees -= 180;
-            x = Math.cos(test);
-            y = Math.sin(test);
-            double temp =  x;
-            x = y;
-            y = temp;
-        }
-        else if (degrees > 90) {
-            degrees -= 90;
-            x = Math.cos(test);
-            y = Math.sin(test);
-            double temp =  x;
-            x = y;
-            y = temp;
-
         } else {
-            x = Math.cos(test);
-            y = Math.sin(test);
-            double temp =  x;
-            x = y;
-            y = temp;
+            if (degrees > 270) degrees -= 270;
+            else if (degrees > 180) degrees -= 180;
+            else if (degrees > 90) degrees -= 90;
         }
+        x = Math.cos(test);
+        y = Math.sin(test);
+        double temp =  x;
+        x = y;
+        y = temp;
+
+
         out.printf("Minute hand is at (x, y): %.2f, %.2f", x, y);
 
     }
