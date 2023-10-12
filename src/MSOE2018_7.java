70e8d08311b6ba27883416c8d083b883c05ac109
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2023-10-12 08:03:46 -0500

    Update

--- src/MSOE2018_7.java
+++ src/MSOE2018_7.java
@@ -1,7 +1,11 @@
 // Alex Nouansacksy
 // MSOE 2018 Question 7
 // 10/12/2023
-
+import java.util.*;
+import static java.lang.System.out;
 
 public class MSOE2018_7 {
+    public static void main(String[] args) {
+
+    }
 }
