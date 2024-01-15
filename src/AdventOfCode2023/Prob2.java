6179911ee3b3210c0b6302062414cda3b4f31582
Alex <alexnouansacksy@gmail.com>
2024-01-15 15:33:59 -0600

    Update

--- src/AdventOfCode2023/Prob2.java
+++ src/AdventOfCode2023/Prob2.java
@@ -1,3 +1,6 @@
+// Alex Nouansacksy
+// Advent 2 Marble Bag (red green blue)
+// 1/15/2024
 package AdventOfCode2023;
 
 import java.io.*;
@@ -9,35 +12,45 @@ public class Prob2 {
     public static void main(String[] args) {
         try {
             Scanner keyboard = new Scanner(new File("src/AdventOfCode2023/prob2.txt"));
-            int tot = 0;
-            while (keyboard.hasNext()) {
-                String junk = keyboard.next();
-                int gameNumber = keyboard.nextInt();
-                junk = keyboard.next();
+            int sum = 0;
+            int gameNum = 0;
+
+            ArrayList<String> games = new ArrayList<>();
+
+            while (keyboard.hasNextLine()) {
                 String a = keyboard.nextLine();
-                Boolean ans = true;
-                String[] list = a.split(";");
-                for (int i = 0; i < list.length; i++) {
-                    String b = list[i];
-                    String[] wow = b.split(",");
-                    for (int j = 0; j < wow.length; j++) {
-                        String d = wow[j];
-                        String[] fred = d.split(" ");
-                        int nn = Integer.parseInt(d.substring(0, 1));
-                        if (fred[1].equals("Red")) {
-
-                        }
-
-
-
-                    }
-                }
+                games.add(a);
             }
 
+            for (String a: games) {
+                gameNum++;
+                boolean validGame = true;
+                String[] round = a.split(";");
+                for (int i = 0; i < round.length; i++) {
+                    round[i] = round[i].replaceAll(":", ",");
+                    String[] marbles = round[i].split(",");
 
+                    for (int j = 1; j < marbles.length; j++) {
+                        String[] marblesCount = marbles[j].split(" ");
+                        int numMarbles = Integer.parseInt(marblesCount[1]);
+                        if (marbles[j].contains("red")) if (numMarbles > 12) validGame = false;
+                        if (marbles[j].contains("blue")) if (numMarbles > 14) validGame = false;
+                        if (marbles[j].contains("green")) if (numMarbles > 13) validGame = false;
+                    }
 
+                }
+                if (!validGame) sum += gameNum;
+
+            }
+
+            out.println(sum);
         } catch (IOException e) {
             out.println("Can't find data file!");
         }
     }
 }
+/* output
+2216
+
+Process finished with exit code 0
+ */
