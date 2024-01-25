da4219bca6c7977771844180971d2c8b7246f0a0
Alex Nouansacksy <alexnouansacksy@gmail.com>
2024-01-25 08:46:05 -0600

    Update

--- src/Prog285bStack/Prog285bStack.java
+++ src/Prog285bStack/Prog285bStack.java
@@ -26,13 +26,8 @@ public class Prog285bStack {
                 claire.setcom();
                 stack.push(claire);
             }
-
             out.println("Number\tCode\tSales\tCommission");
-            Iterator<node> claire = stack.iterator();
-            while (claire.hasNext()){
-                node temp = claire.next();
-                if (temp.getcom() != 0) out.println(temp);
-            }
+            while (!stack.isEmpty()){out.println(stack.pop());}
 
         } catch (IOException e) {
             out.println("Can't find data file!");
@@ -43,17 +38,19 @@ public class Prog285bStack {
 
 /* output
 Number	Code	Sales	Commission
-101		17		2250.0	213.75
-103		5		4000.0	300.0
-118		8		7350.0	574.75
-125		5		6500.0	502.5
-138		17		6375.0	677.5
-192		8		8125.0	640.625
-203		8		3250.0	243.75
-218		5		5000.0	375.0
-235		5		5250.0	396.25
-264		17		4150.0	410.5
 291		17		750.0	71.25
+264		17		4150.0	410.5
+235		5		5250.0	396.25
+218		5		5000.0	375.0
+203		8		3250.0	243.75
+192		8		8125.0	640.625
+138		17		6375.0	677.5
+125		5		6500.0	502.5
+118		8		7350.0	574.75
+117		3		7350.0	0.0
+103		5		4000.0	300.0
+101		17		2250.0	213.75
 
 Process finished with exit code 0
+
  */
