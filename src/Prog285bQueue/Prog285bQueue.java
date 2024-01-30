7d83a5d9089b9d71a4eeb78dd9f6a23414c614ca
Alex Nouansacksy <alexnouansacksy@gmail.com>
2024-01-30 09:25:05 -0600

    Update

--- src/Prog285bQueue/Prog285bQueue.java
+++ src/Prog285bQueue/Prog285bQueue.java
@@ -7,18 +7,13 @@ package Prog285bQueue;
 import java.io.*;
 import java.util.*;
 
-import static java.lang.System.in;
 import static java.lang.System.out;
 
 public class Prog285bQueue {
     public static void main(String[] args) {
         try {
             Scanner keyboard = new Scanner(new File("data/prog285b.dat"));
-            Scanner input = new Scanner(System.in);
-
-            out.print("Queue Length: ");
-            int length = input.nextInt();
-            queueClass wow = new queueClass(length);
+            Queue<node> wow = new LinkedList<node>();
 
             while (keyboard.hasNext()) {
                 int id = keyboard.nextInt();
@@ -26,14 +21,33 @@ public class Prog285bQueue {
                 double sales = keyboard.nextDouble();
 
                 node claire = new node(id, code, sales);
-                queueClass.enqueue(claire);
+                claire.setcom();
+                wow.add(claire);
             }
 
-            queueClass.displayQueue();
+            Iterator<node> iterator = wow.iterator();
+            while(iterator.hasNext()) out.println(iterator.next());
+
+
         } catch (IOException e) {
             out.println("Can't find data file!");
         }
     }
 }
 
+/*
+101 17 2250.0 213.75
+103 5 4000.0 300.0
+117 3 7350.0 0.0
+118 8 7350.0 574.75
+125 5 6500.0 502.5
+138 17 6375.0 677.5
+192 8 8125.0 640.625
+203 8 3250.0 243.75
+218 5 5000.0 375.0
+235 5 5250.0 396.25
+264 17 4150.0 410.5
+291 17 750.0 71.25
 
+Process finished with exit code 0
+ */
