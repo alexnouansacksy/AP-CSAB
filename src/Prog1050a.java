8d423aa92be3e27582823bd1fb2871fc4b2e3ef3
Alex Nouansacksy <alexnouansacksy@gmail.com>
2024-04-02 09:43:32 -0500

    Update

--- src/Prog1050a.java
+++ src/Prog1050a.java
@@ -22,13 +22,35 @@ public class Prog1050a {
             // How many sales were made to Europe?
             int cnt = 0;
             for (String[] strings : list) if (strings[0].equals("Europe")) cnt++;
-            out.println("How many sales were made to Europe?\n" + cnt);
+            out.println("\nHow many sales were made to Europe?\n" + cnt);
 
             // How many items of Cereal were bought by Cambodia?
             cnt = 0;
             for (String[] strings : list)
                 if (strings[0].equals("Europe") && strings[2].equals("Cereal")) cnt+= Integer.parseInt(strings[8]);
-            out.println("How many items of Cereal were bought by Cambodia?\n" + cnt);
+            out.println("\nHow many items of Cereal were bought by Cambodia?\n" + cnt);
+
+            // What was the profit on all of the meat?
+            double total = 0;
+            for (int i = 1; i < list.size(); i++) {
+                String[] strings = list.get(i);
+                total+= Double.parseDouble(strings[13]);
+            }
+            out.println("\nWhat was the profit on all of the meat?\n" + total);
+
+            // What percentage of the sales were High Priority?
+            int hcnt = 0;
+            for (String[] strings : list) {
+                if (strings[4].equals("H")) hcnt++;
+                cnt++;
+            }
+            out.println("\n What percentage of the sales were High Priority?\n" + hcnt + "/" + cnt + " or ");
+            out.printf("%.2f", ((double) hcnt/cnt) * 10);
+            out.print("%");
+
+
+
+
 
         } catch (IOException e) {
             out.println("Can't find data file!");
